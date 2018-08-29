111152042fd41f6fd6ab468711eab5eb365ed53f
Moon Child <kaitline.moon@gmail.com>
2018-08-29 18:03:51 -0400

    JS was completed and the game works! Yay!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,35 +8,58 @@ var guessAmount = 8;
 // Functions for updating the score
 var win = function() {
     wins++;
+    document.querySelector("#wins").innerHTML = wins;
     alert("You did it!");
 }
 
 var lose = function() {
     losses++;
+    document.querySelector("#losses").innerHTML = losses;
     alert("You can't out smart me!")
 }
 
 // Function for counting down the amount of guesses remaining for the player
 var reduce = function() {
-    document.querySelector("#remaining").innerHTML = guessAmount--;
+    guessAmount--;
+    document.querySelector("#remaining").innerHTML = guessAmount;
 }
 
 // Function to add the guessed keys to the "Your Guesses:" area
 var yourGuess = function() {
-    document.querySelector("#player-guess").innerHTML = playerGuess.append(event.key);
+    playerGuess = event.key;
+    var box = document.getElementById("player-guess");
+    var guess = document.createTextNode(playerGuess);
+    box.appendChild(guess);
 }
+
+// Function to generate a random number for the computer
+var computer = function() {
+    computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+
+// Function to reset the game once there has been a win or a loss
+var reset = function() {
+    computer();
+    document.querySelector("#player-guess").innerHTML = "";
+    guessAmount = 8;
+}
+
+
+// Running computer function
+computer();
 
 // Function that runs when a key is pressed
 document.onkeyup = function(event) {
-    playerGuess = event.key;
-    var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-    if (playerGuess !== computerGuess && guessAmount > 8) {
+
+    if (playerGuess !== computerGuess && guessAmount > 0) {
         reduce();
         yourGuess();
     } else if (playerGuess === computerGuess && guessAmount >= 0) {
         win();
+        reset();
     } else {
         lose();
+        reset();
     }
 }
